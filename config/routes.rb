f91bf3370b208474c5418066d2dbcbb60cfe304e
Henry Tabima <henrytabima.dev@gmail.com>
2021-01-11 18:50:33 -0500

    Set the versioning namespaces for API

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # API Definition
   namespace :api, defaults: { format: :json } do
-    # We are going to lists our resources here
+    namespace :v1 do
+      # We are going to lists our resources here
+    end
   end
 end
